88e612fc0bd9e5a5add2c9b375cf5003610627bc
wardanouman <wardanouman82@gmail.com>
2026-09-08 18:38:50 +0500

    chnges

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json());
